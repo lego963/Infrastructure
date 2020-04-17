0fdf6f0cde2ffc3c5e60b657b6ee3401f5cc67c5
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-04-17 14:32:37 +0300

    additional variables add

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,5 +1,9 @@
 module "srv" {
   source = "./modules"
 
-  cloud = "otc"
+  cloud       = "otc"
+  
+  public_key  = var.public_key
+  net_address = var.net_address
+  vpc_name    = var.vpc_name
 }
